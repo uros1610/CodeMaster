76f7a8c9f289bfe94609c61aba2a14a3682f1a2a
Uroš Milović <urkemilovic16@gmail.com>
2024-05-14 20:13:25 +0200

    sredjeni submissionsi

--- backend/routes/submissions.js
+++ backend/routes/submissions.js
@@ -2,9 +2,10 @@ const express = require('express')
 const {expressjwt} = require('express-jwt')
 
 const router = express.Router()
-const {getSubmissionByID, getAllSubmissionsOneUser} = require('../controllers/submissions')
+const {getSubmissionByID, getAllSubmissionsOneUser,insertSubmission} = require('../controllers/submissions')
 
+router.get('/:name/:id',getSubmissionByID)
 router.get('/:name',getAllSubmissionsOneUser)
-router.get('/:name/:id',getAllSubmissionsOneUser)
+router.post('/',insertSubmission)
 
 module.exports = router
