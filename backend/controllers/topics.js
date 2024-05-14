bf5bc2ec57f436eeedc7ceb4e183691a5b9eb9a1
Uroš Milović <urkemilovic16@gmail.com>
2024-05-14 20:13:16 +0200

    sql upit za izvlacenje tema

---
+++ backend/controllers/topics.js
@@ -0,0 +1,27 @@
+const db = require("../db")
+
+
+const getTopic = (req,res) => {
+
+    const topic_name = req.params.topicName;
+
+    console.log(topic_name)
+    const q = "SELECT * FROM Topic WHERE topic_name = ?";
+
+    db.query(q,[topic_name],(err,data) => {
+        if(err) {
+            return res.status(500).json({message:"Internal server error"});
+        }
+        
+        console.log(data.length)
+
+        if(data.length === 0) {
+            return res.status(403).json({message:"The topic isn't available,please choose another"})
+        }
+
+        return res.status(200).json("OK");
+    })
+}
+
+
+module.exports = {getTopic}
