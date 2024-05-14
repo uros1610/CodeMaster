d1a2e5a6a083f742af871a2cbb16b7408b24373c
Uroš Milović <urkemilovic16@gmail.com>
2024-05-14 20:08:28 +0200

    izvlacenje submissionsa iz baze

--- frontend/src/components/Submissions.jsx
+++ frontend/src/components/Submissions.jsx
@@ -3,12 +3,17 @@ import { ProfileNavBar } from './ProfileNavBar'
 import { useState,useEffect} from 'react'
 import axios from 'axios'
 import AuthContext from '../context/AuthContext'
+import Submission from './Submission'
+import styles from '../styles/submissions.css'
+import { useParams } from 'react-router-dom'
 
 const Submissions = () => {
 
   const [data,setData] = useState([])
 
   const {user} = useContext(AuthContext)
+
+  const {username} = useParams()
 
   const BASE_URL = process.env.REACT_APP_BASE_URL
 
@@ -17,7 +22,8 @@ const Submissions = () => {
     const fetchData = async () => {
       try {
         
-      const response = await axios.get(`${BASE_URL}/${user.username}`)
+      const response = await axios.get(`${BASE_URL}/submissions/${username}`)
+      console.log(response)
       setData(response.data)
       }
       catch(err) {
@@ -32,6 +38,16 @@ const Submissions = () => {
   return (
     <div className='profileContainer'>
         <ProfileNavBar/>
+        <table className = "submissionsTable">
+          <tr>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Username</th>
+            <th>Problem Name</th>
+            <th>Verdict</th>
+          </tr>
+          {data.map(item => <Submission item = {item}/>)}
+        </table>
     </div>
   )
 }
