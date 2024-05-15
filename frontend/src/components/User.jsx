25c475cb1454e5368a733b3090113bc9e9264e56
Uroš Milović <urkemilovic16@gmail.com>
2024-05-15 02:23:36 +0200

    dodao user komponentu

---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import styles from '../styles/user.css'
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const User = ({currUser,users,setUsers,role}) => {
+
+    const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+    const [selectedValue, setSelectedValue] = useState(role);
+    const navigate = useNavigate();
+
+  const handleRadioChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+
+
+    const handleClick = async (id) => {
+        try {
+            await axios.delete(`${BASE_URL}/user/${id}`)
+            setUsers(users.filter(user => user.username !== currUser))
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+
+    const handleEditRole = async (username) => {
+
+        try {
+        const resp = await axios.put(`${BASE_URL}/user/updateRole/${username}/${selectedValue}`)
+        alert("Uspjesno editovano!");
+        }
+        catch(err) {
+
+        }
+    }
+
+  return (
+    <div style = {{
+        display:'flex',
+        justifyContent:'space-between',
+        alignItems:'center',
+        border:'2px solid #DDE6ED',
+        color:'#DDE6ED',
+        padding:'10px',
+        width:'100%'
+    }}>{currUser}
+
+    <div className = "mainDivForEdits">
+    
+    <form className = "mainFormForEdits" >
+    <label htmlFor = "Admin">Admin</label>
+    <input className = "radioBtns" name = {`nameRole${currUser}`} type = "radio" value = "Admin" checked = {selectedValue === "Admin"}
+    
+    onChange = {(e) => {handleRadioChange(e,"Admin")}}
+    />
+    
+    <label htmlFor = "Editor">Editor</label>
+    <input className = "radioBtns" name = {`nameRole${currUser}`} type = "radio" value = "Editor" checked = {selectedValue === "Editor"}
+    
+    onChange = {(e) => {handleRadioChange(e,"Editor")}}
+    />
+   
+
+   
+    <label htmlFor = "User">User</label>
+    <input className = "radioBtns" name = {`nameRole${currUser}`} type = "radio" value = "User" checked = {selectedValue === "User"}
+    
+    onChange = {(e) => {handleRadioChange(e,"User")}}
+    />
+    </form>
+
+   
+
+    
+    <button className = "buttonEdit"
+    onClick = {(e) => {handleEditRole(currUser)}}
+    >Edit Role</button>
+    <button className = "buttonDelete" 
+
+    onClick={(e) => {handleClick(currUser)}}
+
+    >Delete</button>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default User
