81fc396fff838b6d3e61993c78b11723cf237779
Uroš Milović <urkemilovic16@gmail.com>
2024-05-16 01:30:47 +0200

    ispravljene rute

--- backend/routes/inputoutput.js
+++ backend/routes/inputoutput.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
-const insertIO = require('../controllers/inputoutput')
+const {insertIO,getInputs,getOutputs} = require('../controllers/inputoutput')
 
-
+router.get("/input/:name",getInputs)
+router.get("/output/:name",getOutputs)
 router.post('/',insertIO)
+
 
 module.exports = router
