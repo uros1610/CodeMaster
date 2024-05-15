6a1e9ce753ee5d7a7d33d86451c99e3948d3a650
Uroš Milović <urkemilovic16@gmail.com>
2024-05-16 01:30:56 +0200

    ispravljene rute

--- backend/routes/problem.js
+++ backend/routes/problem.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
-const {getProblemByName,allProblems,addProblem} = require('../controllers/problem')
+const {getProblemByName,allProblems,addProblem,getInputs,getOutputs} = require('../controllers/problem')
 
-
+router.get('/singleproblem/:name',getProblemByName)
 router.get('/problemset',allProblems)
-router.get('/:name',getProblemByName)
 
 router.post('/',addProblem)
+router.get("/inputs/:name",getInputs)
+router.get("/outputs/:name",getOutputs)
 
 module.exports = router
