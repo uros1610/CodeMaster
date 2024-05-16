6daac101ae7a3b103a16f9c03485c7bee67c7b4e
Uroš Milović <urkemilovic16@gmail.com>
2024-05-16 18:00:10 +0200

    ruta za count za probleme

--- backend/routes/problem.js
+++ backend/routes/problem.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const {getProblemByName,allProblems,addProblem,getInputs,getOutputs} = require('../controllers/problem')
+const {getProblemByName,allProblems,addProblem,getInputs,getOutputs,getCount} = require('../controllers/problem')
 
+router.get('/problemCount',getCount)
 router.get('/singleproblem/:name',getProblemByName)
-router.get('/problemset',allProblems)
+router.get('/problemset/:id',allProblems)
 
 router.post('/',addProblem)
 router.get("/inputs/:name",getInputs)
