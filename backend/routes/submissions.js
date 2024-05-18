266ffa4dad3b7a0ee38e17047d0ae06efb170546
Uroš Milović <urkemilovic16@gmail.com>
2024-05-18 17:49:42 +0200

    nova ruta za sve submissione prihvacene

--- backend/routes/submissions.js
+++ backend/routes/submissions.js
@@ -2,8 +2,9 @@ const express = require('express')
 const {expressjwt} = require('express-jwt')
 
 const router = express.Router()
-const {getSubmissionByID, getAllSubmissionsOneUser,insertSubmission} = require('../controllers/submissions')
+const {getSubmissionByID, getAllSubmissionsOneUser,insertSubmission,getAllSubmissionsOneUserAccepted} = require('../controllers/submissions')
 
+router.get('/accepted/:name',getAllSubmissionsOneUserAccepted)
 router.get('/:name/:id',getSubmissionByID)
 router.get('/:name',getAllSubmissionsOneUser)
 router.post('/',insertSubmission)
