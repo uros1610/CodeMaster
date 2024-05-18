8ddf430127159ab01ae946cb1aa20e1fb213c6fe
Uroš Milović <urkemilovic16@gmail.com>
2024-05-18 17:49:17 +0200

    makao printove koji su sluzili za testiranje

--- backend/controllers/submit.js
+++ backend/controllers/submit.js
@@ -7,7 +7,6 @@ async function submitSolution(req, res) {
 
     const { code, date, username, problemname, val } = req.body;
 
-    console.log(val);
 
     const languageMap = {
       "cpp17":'C++ 17',
@@ -18,14 +17,13 @@ async function submitSolution(req, res) {
 
     const submission = {code,date,username,problemname,val:languageMap[val]}
 
-    console.log("BODY",req.body);
 
     const BASE_URL = "http://localhost:8800/backend";
 
 
 
     const inputs = await axios.get(`${BASE_URL}/problem/inputs/${problemname}`);
-    const outputs = await axios.get(`${BASE_URL}/problem/inputs/${problemname}`);
+    const outputs = await axios.get(`${BASE_URL}/problem/outputs/${problemname}`);
 
 
     const CLIENT_ID = process.env.CLIENT_ID;
@@ -57,27 +55,29 @@ async function submitSolution(req, res) {
         const response = await axios.post("https://api.jdoodle.com/v1/execute", data,headers);
         console.log("DATAAAAA", response.data);
 
-        if (response.data.output !== outputs.data[0].value) {
+        if (response.data.output !== outputs.data[i].value) {
           submission.verdictdescription = `Wrong answer on test case ${i+1}`
+          console.log(response.data.output,outputs.data[i].value);
           await axios.post(`${BASE_URL}/submissions/`, submission);
           return res.status(404).json({"message":`Wrong answer on test case ${i+1}`})
           
         }
+
       } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
-        return;
+        submission.verdictdescription = `Wrong answer on test case ${i+1}`
+        await axios.post(`${BASE_URL}/submissions/`, submission);
+        return res.status(200).json(submission);
       }
-    
     }
+
+
     submission.verdictdescription = 'Accepted'
     await axios.post(`${BASE_URL}/submissions/`, submission);
+    return res.status(200).json(submission);}
 
-   return res.status(200).json({ message: 'Accepted'});
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+    catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+      }
 }
 
 module.exports = {submitSolution};
