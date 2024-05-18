22033b6b94e8f1db14c6a57c249f2895ec2384ef
Uroš Milović <urkemilovic16@gmail.com>
2024-05-18 17:47:20 +0200

    dodat dio da se kod vidi kad se klikne na id

--- frontend/src/components/Submission.jsx
+++ frontend/src/components/Submission.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { FaWindowClose } from 'react-icons/fa'
 
 const Submission = ({item}) => {
+
+  const [display,setDisplay] = useState(false)
+
+  const handleClick1 = () => {
+    setDisplay(true)
+  }
+
+  const handleClick2 = () => {
+    setDisplay(false)
+  }
+
   return (
+    <>
     <tr key = {item.id}>
-        <td>{item.id}</td>
+        <td><button className = "submissionIDbtn" onClick={handleClick1}>{item.id}</button></td>
         <td>{new Date(item.date).toLocaleString()}</td>
-        <td>{item.userName}</td>
-        <td>{item.problemTitle}</td>
+        <td><Link to = {`/profile/${item.userName}`} className = "problemTitle">{item.userName}</Link> </td>
+        <td><Link to = {`/problem/${item.problemTitle}`} className = "problemTitle">{item.problemTitle}</Link> </td>
         <td>{item.language}</td>
         <td style = {{
           color:item.verdictdescription === "Accepted" ? 'lime' : 'red'
         }}>{item.verdictdescription}</td>
     </tr>
+
+      {display && <div className = "displayCode" readOnly>
+        <button className = "closeWindow" onClick = {handleClick2}><FaWindowClose/></button>
+        <textarea className = "textareacode" readOnly>
+        {item.code}
+        </textarea>
+        
+        
+        </div>}
+
+    </>
     )
 }
 
