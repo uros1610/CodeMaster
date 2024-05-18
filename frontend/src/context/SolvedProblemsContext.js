e8e2f4f67a6ebab48b42522659770c8472706a20
Uroš Milović <urkemilovic16@gmail.com>
2024-05-18 17:47:46 +0200

    dodat kontekst za rijesene probleme

---
+++ frontend/src/context/SolvedProblemsContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState,useContext,useEffect} from 'react';
+import AuthContext from './AuthContext';
+import axios from 'axios';
+
+export const SolvedProblemsContext = createContext();
+
+export const SolvedProblemsProvider = ({ children }) => {
+    const [solvedProblems, setSolvedProblems] = useState([]);
+    const {user} = useContext(AuthContext)
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+          try {
+            
+          const response = await axios.get(`/submissions/accepted/${user.username}`)
+          setSolvedProblems(response.data)
+          }
+          catch(err) {
+            console.log(err)
+          }
+        }
+    
+        fetchData()
+    
+      },[])
+    
+
+    return (
+        <SolvedProblemsContext.Provider value={{ solvedProblems, setSolvedProblems }}>
+            {children}
+        </SolvedProblemsContext.Provider>
+    );
+};
