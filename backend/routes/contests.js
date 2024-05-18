d9ee01c85fd599aae72339e0c9b00e3672e83db2
Uroš Milović <urkemilovic16@gmail.com>
2024-05-18 17:49:30 +0200

    ruta za takmicenja,usere i leaderboard

--- backend/routes/contests.js
+++ backend/routes/contests.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const router = express.Router()
-const {createContest,getAllContests,deleteContest} = require('../controllers/contest.js')
+const {createContest,getAllContests,deleteContest,getProblemsByContest, getUsersByContest} = require('../controllers/contest.js')
 const {verifyRoles} = require('../middleware/verifyRoles.js')
 
+router.get('/:contestName/users/:id',getUsersByContest)
+router.get('/:contestName/standings',getProblemsByContest)
 router.post('/addcontest',createContest)
 router.get('/',getAllContests)
 router.delete('/delete/:name',deleteContest)
