f9bfb2ad6a3c07d566fba4b69e7c4ffd64d9ac0b
Uroš Milović <urkemilovic16@gmail.com>
2024-05-21 13:04:00 +0200

    modifikovao rutu

--- backend/routes/submissions.js
+++ backend/routes/submissions.js
@@ -4,7 +4,7 @@ const {expressjwt} = require('express-jwt')
 const router = express.Router()
 const {getSubmissionByID, getAllSubmissionsOneUser,insertSubmission,getAllSubmissionsOneUserAccepted} = require('../controllers/submissions')
 
-router.get('/accepted/:name',getAllSubmissionsOneUserAccepted)
+router.get('/accepted',getAllSubmissionsOneUserAccepted)
 router.get('/:name/:id',getSubmissionByID)
 router.get('/:name',getAllSubmissionsOneUser)
 router.post('/',insertSubmission)
