e29e5aaef0f56ef20d3d47d35865ca0c2ee15119
Uroš Milović <urkemilovic16@gmail.com>
2024-05-21 13:03:38 +0200

    dodao nove rute

--- backend/routes/contests.js
+++ backend/routes/contests.js
@@ -1,13 +1,17 @@
 const express = require('express')
 
 const router = express.Router()
-const {createContest,getAllContests,deleteContest,getProblemsByContest, getUsersByContest} = require('../controllers/contest.js')
+const {createContest,getAllContests,deleteContest,getProblemsByContest, getUsersByContest,registerUser,getContestsUser,deleteUserFromContest,updateUserProblem} = require('../controllers/contest.js')
 const {verifyRoles} = require('../middleware/verifyRoles.js')
 
+router.post("/register",registerUser)
+router.put("/:contestName/:userName",updateUserProblem)
+router.delete("/delete/:contestName/:userName",deleteUserFromContest)
 router.get('/:contestName/users/:id',getUsersByContest)
 router.get('/:contestName/standings',getProblemsByContest)
 router.post('/addcontest',createContest)
 router.get('/',getAllContests)
 router.delete('/delete/:name',deleteContest)
+router.get("/:contestName/:userName",getContestsUser)
 
 module.exports = router
