a849f4355fa9c290a1a8850b1bbe7dcc3a33ff33
Uroš Milović <urkemilovic16@gmail.com>
2024-05-22 20:04:42 +0200

    nove rute

--- backend/routes/contests.js
+++ backend/routes/contests.js
@@ -1,17 +1,18 @@
 const express = require('express')
 
 const router = express.Router()
-const {createContest,getAllContests,deleteContest,getProblemsByContest, getUsersByContest,registerUser,getContestsUser,deleteUserFromContest,updateUserProblem} = require('../controllers/contest.js')
+const {createContest,getAllContests,deleteContest,getProblemsByContest, getUsersByContest,registerUser,getContestsUser,deleteUserFromContest,updateUserProblem,setProcessedTrue} = require('../controllers/contest.js')
 const {verifyRoles} = require('../middleware/verifyRoles.js')
 
 router.post("/register",registerUser)
 router.put("/:contestName/:userName",updateUserProblem)
 router.delete("/delete/:contestName/:userName",deleteUserFromContest)
 router.get('/:contestName/users/:id',getUsersByContest)
-router.get('/:contestName/standings',getProblemsByContest)
+router.get('/:contestName/problems',getProblemsByContest)
 router.post('/addcontest',createContest)
 router.get('/',getAllContests)
 router.delete('/delete/:name',deleteContest)
 router.get("/:contestName/:userName",getContestsUser)
+router.put("/updateProcessed",setProcessedTrue);
 
 module.exports = router
