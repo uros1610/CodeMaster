ed4dce2160610b40d87d6dedb1f54b1b43cb121e
Uroš Milović <urkemilovic16@gmail.com>
2024-05-22 20:04:52 +0200

    update rating-a za usere

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 require('dotenv').config()
 
-const {getUser, getUsersRating,getUsers,deleteUser,filterUsers,noUsers, updateRole} = require('../controllers/user')
+const {getUser, getUsersRating,getUsers,deleteUser,filterUsers,noUsers, updateRole,updateRating} = require('../controllers/user')
 
 router.get("/allusersCount",noUsers)
 router.get("/rating/:id",getUsersRating)
@@ -14,6 +14,7 @@ router.delete("/:username",deleteUser)
 router.get("/filteredusers/:id",filterUsers);
 router.get("/:username",getUser)
 router.put("/updateRole/:username/:rola",updateRole);
+router.put("/:username/rating",updateRating)
 
 
 
