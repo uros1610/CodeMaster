c91016cb7e4e78c3fd7794c142ab5adee2963fad
Uroš Milović <urkemilovic16@gmail.com>
2024-05-24 22:25:18 +0200

    dodat interceptor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
 import { ProblemsContextProvider } from './context/ProblemsContext';
 import { SolvedProblemsProvider } from './context/SolvedProblemsContext';
+import setupInterceptors from './interceptor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+setupInterceptors();
 root.render(
   <React.StrictMode>
     <Router>
