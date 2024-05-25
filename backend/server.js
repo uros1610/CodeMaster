ead7b4b29b706652bef176a4ffde55246f3a6e92
Uroš Milović <urkemilovic16@gmail.com>
2024-05-25 19:33:15 +0200

    funkcija za izracunavanje rejtinga

--- backend/server.js
+++ backend/server.js
@@ -52,23 +52,44 @@ const checkForNewContests = () => {
     });
 };
 
+
+const K = 32;
+
+const calculateEloRating = (R_A, R_B, S_A) => {
+    const E_A = 1 / (1 + 10 ** ((R_B - R_A) / 400));
+    const new_R_A = R_A + K * (S_A - E_A);
+    return new_R_A;
+};
+
 const updateRatings = async (name) => {
-    await axios.put(`http://localhost:8800/backend/contest/updateProcessed`,{name:name})
-    const resp = await axios.get(`http://localhost:8800/backend/contest/${name}/users/1`)
-    const users = [...resp.data]
+    await axios.put(`http://localhost:8800/backend/contest/updateProcessed`, { name: name });
+    const resp = await axios.get(`http://localhost:8800/backend/contest/${name}/users/1`);
+    const users = [...resp.data];
 
     const total = users.length;
 
-    users.sort((a,b) => b.ranking - a.ranking)
+    users.sort((a, b) => b.ranking - a.ranking);
 
-    users.forEach(async (user,index) => {
+    for (let i = 0; i < total; i++) {
+        const user_A = users[i];
+        const R_A = user_A.rating;
+        let new_R_A = R_A;
 
-        const newRating = user.rating + 15 * (total-index-1);
-        const ratingChange = newRating - user.rating;
-        await axios.put(`http://localhost:8800/backend/contest/${user.username}/${name}/updateRatingChange`,{ratingChange,placed:index+1})
-        await axios.put(`http://localhost:8800/backend/user/${user.username}/rating`,{newRating})
-    })
-}
+        for (let j = 0; j < total; j++) {
+            if (i !== j) {
+                const user_B = users[j];
+                const R_B = user_B.rating;
+                const S_A = (user_A.points > user_B.points) ? 1 : (user_A.points === user_B.points ? 0.5 : 0);
+                new_R_A = calculateEloRating(new_R_A, R_B, S_A);
+            }
+        }
+
+        const ratingChange = new_R_A - R_A;
+        await axios.put(`http://localhost:8800/backend/contest/${user_A.username}/${name}/updateRatingChange`, { ratingChange, placed: i+1 });
+        await axios.put(`http://localhost:8800/backend/user/${user_A.username}/rating`, { newRating: new_R_A });
+    }
+};
+
 
 const scheduleContestEndJob = (contest) => {
     const endTime = new Date(new Date(contest.date).getTime()+contest.length*60*1000);
