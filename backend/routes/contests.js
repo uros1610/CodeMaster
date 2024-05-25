3caa079440133cbe8a8b1018c5c3c8557dcdd290
Uroš Milović <urkemilovic16@gmail.com>
2024-05-25 10:32:03 +0200

    dvije nove rute

--- backend/routes/contests.js
+++ backend/routes/contests.js
@@ -1,12 +1,16 @@
 const express = require('express')
 
 const router = express.Router()
-const {createContest,getAllContests,deleteContest,getProblemsByContest, getUsersByContest,registerUser,getContestsUser,deleteUserFromContest,updateUserProblem,setProcessedTrue} = require('../controllers/contest.js')
+const {createContest,getAllContests,deleteContest,getProblemsByContest, getUsersByContest,registerUser,getContestsUser,deleteUserFromContest,updateUserProblem,setProcessedTrue,
+updateRatingChange} = require('../controllers/contest.js')
 const {expressjwt} = require('express-jwt');
 require('dotenv').config()
 const {verifyAdmin} = require('../middleware/verifyToken')
 
-router.use(expressjwt({ secret: process.env.SECRET_KEY, algorithms: ['HS256']}));
+router.put("/:userName/:contestName/updateRatingChange",updateRatingChange);
+router.put("/updateProcessed",setProcessedTrue);
+
+
 
 
 router.post("/register",registerUser)
@@ -14,10 +18,11 @@ router.put("/:contestName/:userName",updateUserProblem)
 router.delete("/delete/:contestName/:userName",deleteUserFromContest)
 router.get('/:contestName/users/:id',getUsersByContest)
 router.get('/:contestName/problems',getProblemsByContest)
-router.post('/addcontest',verifyAdmin,createContest)
-router.get('/',getAllContests)
 router.delete('/delete/:name',verifyAdmin,deleteContest)
 router.get("/:contestName/:userName",getContestsUser)
-router.put("/updateProcessed",setProcessedTrue);
+
+router.post('/addcontest',verifyAdmin,createContest)
+router.get('/',getAllContests)
+
 
 module.exports = router
