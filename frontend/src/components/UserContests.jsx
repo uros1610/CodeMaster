65de10d3724ce2ea6fdbfb296d7f6333f1eec8b0
Uroš Milović <urkemilovic16@gmail.com>
2024-05-25 10:33:25 +0200

    nova komponenta

--- frontend/src/components/UserContests.jsx
+++ frontend/src/components/UserContests.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import { ProfileNavBar } from './ProfileNavBar'
+import { useParams } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const UserContests = () => {
+  const {username} = useParams();
+
+  const [contests,setContests] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/backend/user/contests/${username}`)
+        setContests(response.data)
+        console.log("RESP",response);
+      }
+      catch(err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  },[])
+
   return (
     <div className='profileContainer'>
         <ProfileNavBar/>
+
+        <table className = "submissionsTable">
+
+        <tr>
+          <th>
+            Contest Name
+          </th>
+          <th>Placement</th>
+          <th>Rating change</th>
+        </tr>
+
+        {contests.map(contest => (<tr>
+          <td><Link className = "linksNavBar" to = {`/contest/${contest.contestName}/standings`}>{contest.contestName}</Link></td>
+          <td>{contest.placed}</td>
+          <td>{contest.ratingGain}</td>
+        </tr>))}
+
+        </table>
+
+
     </div>
   )
 }
