e8de28dda7eb4812a95dabfec89a83dba37b0f83
Uroš Milović <urkemilovic16@gmail.com>
2024-05-25 10:32:14 +0200

    mala izmjena

--- backend/routes/submit.js
+++ backend/routes/submit.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const {submitSolution} = require('../controllers/submit.js');
 const router = express.Router();
-
 const {expressjwt} = require('express-jwt');
 require('dotenv').config()
 
-
-router.use(expressjwt({ secret: process.env.SECRET_KEY, algorithms: ['HS256']}));
-
-router.post("/",submitSolution);
+router.post("/",expressjwt({ secret: process.env.SECRET_KEY, algorithms: ['HS256']}),submitSolution);
 
 module.exports = router;
