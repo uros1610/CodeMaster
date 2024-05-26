6bceb621bc3e8a52af6f7acf5220039120ac0406
Uroš Milović <urkemilovic16@gmail.com>
2024-05-26 18:29:28 +0200

    nova komponenta za loading screen

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Loading = () => {
+    return <div className = "dlo" style = {{
+        margin:'auto auto',
+        fontSize:'60px',
+        color:'#e3fef7',
+        height:'100%',
+        width:'100%',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center'
+    }}><p>Loading,Please wait...</p></div>
+}
+
+export default Loading
