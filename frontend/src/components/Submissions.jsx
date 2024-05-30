dcf70881d44575a1460dfce3674c167356e72285
Uroš Milović <urkemilovic16@gmail.com>
2024-05-30 10:40:36 +0200

    paginacija

--- frontend/src/components/Submissions.jsx
+++ frontend/src/components/Submissions.jsx
@@ -6,33 +6,56 @@ import AuthContext from '../context/AuthContext'
 import Submission from './Submission'
 import styles from '../styles/submissions.css'
 import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import PageNumbers from './PageNumbers'
 
 const Submissions = () => {
 
   const [data,setData] = useState([])
 
   const {user} = useContext(AuthContext)
+  const [no,setNo] = useState(0);
+  const [pageNumber,setpageNumber] = useState(1);
 
   const {username} = useParams()
+  const navigate = useNavigate();
 
+  const fetchData = async () => {
+    try {
+      
+    const response = await axios.get(`/backend/submissions/all/${username}/${pageNumber}`)
+    console.log(response.data);
+      
+    setData(response.data)
+    }
+    catch(err) {
+      
+      console.log(err)
+    }
+  }
+
+  const fetchNo = async () => {
+    try {
+    const response = await axios.get(`/backend/submissions/allSubmissions/count/${username}`);
+    console.log(response.data[0]);
+    setNo(Math.ceil(response.data[0].broj / 20));
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchNo();
+
+  },[])
 
   useEffect(() => {
 
-    const fetchData = async () => {
-      try {
-        
-      const response = await axios.get(`/backend/submissions/${username}`)
-      const sorted = response.data.sort((a,b) => ((a.id < b.id) ? 1 : -1))
-      setData(sorted)
-      }
-      catch(err) {
-        console.log(err)
-      }
-    }
+   
+    fetchData();
 
-    fetchData()
-
-  },[])
+  },[pageNumber])
 
   return (
     <div className='profileContainer'>
@@ -48,6 +71,9 @@ const Submissions = () => {
           </tr>
           {data.map(item => <Submission item = {item}/>)}
         </table>
+
+    <PageNumbers no={no} pageNumber={pageNumber} setpageNumber={setpageNumber}/>
+ 
     </div>
   )
 }
