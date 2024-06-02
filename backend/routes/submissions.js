510c12410d79c6ae08c9d8ba503b78e60df3daf5
Uroš Milović <urkemilovic16@gmail.com>
2024-06-03 00:22:06 +0200

    ruta

--- backend/routes/submissions.js
+++ backend/routes/submissions.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
 const router = express.Router()
-const {getSubmissionByID, getAllSubmissionsOneUser,insertSubmission,getAllSubmissionsAccepted,getAllSubmissionsGrouped, getNoSubmissions} = require('../controllers/submissions')
+const {getSubmissionByID, getAllSubmissionsOneUser,insertSubmission,getAllSubmissionsAccepted,getAllSubmissionsGrouped, getNoSubmissions, getAllSubmissionsOneUserOneProblem} = require('../controllers/submissions')
 
 router.get('/allSubmissions/count/:username',getNoSubmissions);
+router.get('/allSubmissions/:username/:problemname',getAllSubmissionsOneUserOneProblem)
 router.get('/allSubmissionsGrouped/:username',getAllSubmissionsGrouped)
 router.get('/accepted',getAllSubmissionsAccepted)
 router.get('/:name/:id',getSubmissionByID)
