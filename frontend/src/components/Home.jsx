65f49f0456c2ff0718f85768a6409914aa1aff53
Uroš Milović <urkemilovic16@gmail.com>
2024-06-05 18:45:42 +0200

    home komponenta

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from '../styles/home.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <h1 className = "welcome">Welcome to CodeMaster!</h1>
+    <div className = "homeDiv">
+
+      <div className = "quoteDiv">
+        <p>At CodeMaster, we push you to be your best. Tackle diverse problems, compete with talented coders, and enhance your skills. Each challenge is a step toward mastery. Start your coding journey with CodeMaster.</p>
+        <button className = "startSolving"><Link to = "/problemset" className = "solvingLink">Start Solving</Link></button>
+      </div>
+
+      <img src = "/pngegg.png"/>
+        
+      
+    </div>
+    </>
   )
 }
 
