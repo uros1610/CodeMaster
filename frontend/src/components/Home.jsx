ac349707f65fea31c5b0b2de3dd9db9b47654d31
Uroš Milović <urkemilovic16@gmail.com>
2024-06-13 00:07:09 +0200

    navigate do logina ako nije loginovan

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import styles from '../styles/home.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import AuthContext from '../context/AuthContext'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/problemset')
+  }
+
+  const {user} = useContext(AuthContext)
+
+  useEffect(() => {
+
+    if(!user) {
+      navigate('/login');
+    }
+  },[user])
+  
+
   return (
     <>
     <h1 className = "welcome">Welcome to CodeMaster!</h1>
@@ -10,7 +28,7 @@ const Home = () => {
 
       <div className = "quoteDiv">
         <p>At CodeMaster, we push you to be your best. Tackle diverse problems, compete with talented coders, and enhance your skills. Each challenge is a step toward mastery. Start your coding journey with CodeMaster.</p>
-        <button className = "startSolving"><Link to = "/problemset" className = "solvingLink">Start Solving</Link></button>
+        <button className = "startSolving" onClick = {handleClick}><Link to = "/problemset" className = "solvingLink">Start Solving</Link></button>
       </div>
 
       <img src = "/pngegg.png"/>
