d4b43ed61f35d81f2a11e4b6ce6bfa0d446f1195
uros1610 <urkemilovic16@gmail.com>
2024-08-17 23:08:35 +0200

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
-var mysql = require('mysql')
+var mysql = require('mysql2')
+require('dotenv').config()
 
  const dbConf = mysql.createConnection({
-    host:"localhost",
-    user:'root',
-    password:'1234',
-    database:'CompetitiveDB',
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABASE_NAME,
 })
 
 
